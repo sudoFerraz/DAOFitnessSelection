d98a6d3d906185af5feb64acdd0a38c2d7ea2ea5
SudoFerraz <sudoferraz@gmail.com>
2021-12-11 08:13:18 +0200

    Cleaning old scripts

--- scripts/advanced_collectible/create_metadata.py
+++
@@ -1,8 +0,0 @@
-from brownie import AdvancedCollectible
-
-def main():
-    advanced_collectible = AdvancedCollectible[-1]
-    number_of_advanced_collectibles = advanced_collectible.tokenCounter()
-    print(f"You have created {number_of_advanced_collectibles} collectibles!")
-    for token_id in range(number_of_advanced_collectibles):
-        breed = advanced_collectible.tokenIdToBreed(token_id)
--- scripts/advanced_collectible/get_token_counter.py
+++
@@ -1,9 +0,0 @@
-from brownie import AdvancedCollectible
-from scripts.helpful_scripts import fund_with_link, get_account
-from web3 import Web3
-
-def main():
-    advanced_collectible = AdvancedCollectible[-1]
-    tokenCounter = advanced_collectible.tokenCounter()
-    print(f"There are now {tokenCounter} collectibles minted!")
-    print(advanced_collectible)
